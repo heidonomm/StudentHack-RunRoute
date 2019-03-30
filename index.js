b67683d2d89e61e2a138c350faf0c20e77526d89
Heido Nomm <heido.nomm@student.manchester.ac.uk>
2019-03-30 13:23:07 +0000

    reformated working

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
- 
 var path = require('path');
+
+var collect = require('@turf/directions');
  
 const portNumber = process.env.port || 3000;
  
 http.listen(portNumber);
+
+app.use(express.static(__dirname));
  
  
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
  
 console.log("server running on port 3000");
